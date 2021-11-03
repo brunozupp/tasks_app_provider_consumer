531e83d84f54dabef260d1f9e8f0ca77f9e752e5
bruno.noveli <brunimzuppwolf@gmail.com>
2021-11-03 13:39:36 -0300

    Estrutura básica criada

---
+++ lib/pages/login/login_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(""),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child: Column(),
+      ),
+    );
+  }
+}
